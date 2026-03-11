563780bd515f275f572773dd2535f33b6ce34dc8
Darren Sills <darrsills@gmail.com>
2026-03-11 15:38:05 +0000

    whimsy destroyer

--- src/components/content/contact.tsx
+++ src/components/content/contact.tsx
@@ -70,7 +70,7 @@ export default function Contact() {
             id="message"
             name="message"
             required
-            placeholder="Hi Darren, you're so handsome and talented!"
+            placeholder="Your message here..."
             rows={8}
             className="flex-grow bg-base-300 px-3 py-2 rounded border border-base-100 resize-none focus:outline-none focus:ring-2 focus:ring-accent text-base-content"
           />
